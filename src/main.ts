fc48b814e945083ede92225ddf9500cc0aab42c6
Steven Roderick <contact@stevenroderick.com>
2024-02-27 23:03:42 -0600

    Added initial typed usage of promise for waiting for matterport sdk to initialize
    Additionally had to change bundler as Node16 and above implemented new rules on how types are exported from libraries.
    
    Discussion: https://github.com/microsoft/TypeScript/issues/52363

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,18 @@
-import './style.css'
-import '@matterport/webcomponent'
+import './style.css';
+import '@matterport/webcomponent';
 
+const viewer = document.querySelector<MatterportViewer>('matterport-viewer');
+if (viewer) {
+  viewer.addEventListener('mpSdkPlaying', evt => {
+    const mpSdk = evt.detail.mpSdk;
+    mpSdk.Camera.rotate(90, 0);
+  })
+}
+
+const main = async () => {
+  if (viewer) {
+    const mpSdk = await viewer.playingPromise
+    //  mpSdk.Camera.rotate(90, 0);
+  }
+}
+main();
